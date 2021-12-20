243bfae5ace4693073114f013852c11a328e2863
limingjing20011013 <96398785+limingjing20011013@users.noreply.github.com>
2021-12-20 11:27:30 +0800

    Add files via upload

---
+++ proverb.js
@@ -0,0 +1,4 @@
+var proverb = ["Two heads are better than one.三个臭皮匠，顶个诸葛亮",
+"One today is worth two tomorrows.一个今天胜似两个明天",
+"There is no royal road to science ,and only those who do not dread the fatiguing climb of gaining its numinous summits.在科学上没有平坦的大道，只有不畏劳苦沿着其崎 岖之路攀登的人，才有希望达到它光辉的顶点",
+"Life is a show that never ends. Each of us is an actor. However, some people obey themselves, others please the audience.人生是一场永不落幕的演出，我们每一个人都是演员，只不过，有的人顺从自己，有的人取悦观众"];
